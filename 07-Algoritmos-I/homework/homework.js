d05d9b9fe7a5c3bc81f2736dfeb276cdfced2bb4
Agustin David Benitez <agustindavidbenitez1@gmail.com>
2022-06-29 10:56:33 -0300

    clase 7

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -65,7 +65,7 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-  for(let i=0; i<array.length ;i++){
+  for(let i=0; i<array.length-1 ;i++){
     let min = i;
     for(let j= i+1; j<array.length ;j++){
       if(array[j]<array[min]){
